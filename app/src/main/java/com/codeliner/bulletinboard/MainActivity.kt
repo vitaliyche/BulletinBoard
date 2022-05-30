85ea8a38e20a1a884056cc9ba16eb821ac2242ef
vitaliyche <vit.chernyavsky@gmail.com>
2022-05-30 14:07:51 +0300

    Добавил Drawer Menu

--- app/src/main/java/com/codeliner/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/codeliner/bulletinboard/MainActivity.kt
@@ -2,10 +2,52 @@ package com.codeliner.bulletinboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
+
+    private fun init() {
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout_main, toolbar_content, R.string.open, R.string.close) //добавить кнопку для всплывающего меню
+        drawer_layout_main.addDrawerListener(toggle)//указать, что дроуэр лэйаут будет открываться при нажатии на кнопку
+        toggle.syncState()
+        nav_view_main.setNavigationItemSelectedListener (this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.id_my_ads -> {
+                Toast.makeText(this, "Pressed id_my_ads", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_car -> {
+                Toast.makeText(this, "Pressed id_car", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_pc -> {
+                Toast.makeText(this, "Pressed id_pc", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_sign_up -> {
+                Toast.makeText(this, "Pressed id_sign_up", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_sign_in -> {
+                Toast.makeText(this, "Pressed id_sign_in", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_sign_out -> {
+                Toast.makeText(this, "Pressed id_sign_out", Toast.LENGTH_LONG).show()
+            }
+        }
+        drawer_layout_main.closeDrawer(GravityCompat.START)
+        return true
+    } // при нажатии на элемент, получаем item на который нажали
 }
