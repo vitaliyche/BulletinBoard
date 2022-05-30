25f83b481391048c09f39d2298d3385ac148900e
vitaliyche <vit.chernyavsky@gmail.com>
2022-05-30 11:37:23 +0300

    Initial commit

---
+++ app/src/main/java/com/codeliner/bulletinboard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codeliner.bulletinboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
