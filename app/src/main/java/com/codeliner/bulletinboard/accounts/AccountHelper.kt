62c8b3f0d57360bf3a09d0b1af69bf05e52b13e9
vitaliyche <vit.chernyavsky@gmail.com>
2022-06-11 12:10:57 +0300

    Exceptions при регистрации и входе

--- app/src/main/java/com/codeliner/bulletinboard/accounts/AccountHelper.kt
+++ app/src/main/java/com/codeliner/bulletinboard/accounts/AccountHelper.kt
@@ -1,13 +1,14 @@
 package com.codeliner.bulletinboard.accounts
 
+import android.util.Log
 import android.widget.Toast
 import com.codeliner.bulletinboard.MainActivity
 import com.codeliner.bulletinboard.R
+import com.codeliner.bulletinboard.constants.FireBaseAuthConstants
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 
 class AccountHelper(activity: MainActivity) {
     private val activity = activity
@@ -21,7 +22,30 @@ class AccountHelper(activity: MainActivity) {
                         sendEmailVerification(task.result?.user!!)
                         activity.uiUpdate(task.result?.user) //user может быть null, когда не осуществлен вход
                     } else {
-                        Toast.makeText(activity, activity.resources.getString(R.string.sign_up_error), Toast.LENGTH_LONG).show()
+                        Log.d("Log.d", "Exception : ${task.exception}")
+                        if (task.exception is FirebaseAuthUserCollisionException) {
+                            val exception = task.exception as FirebaseAuthUserCollisionException
+                            if (exception.errorCode == FireBaseAuthConstants.ERROR_EMAIL_ALREADY_IN_USE) {
+                                Toast.makeText(activity, FireBaseAuthConstants.ERROR_EMAIL_ALREADY_IN_USE, Toast.LENGTH_LONG).show()
+                            }
+                        } else if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            val exception = task.exception as FirebaseAuthUserCollisionException
+                            if (exception.errorCode == FireBaseAuthConstants.ERROR_INVALID_EMAIL) {
+                                Toast.makeText(activity, FireBaseAuthConstants.ERROR_INVALID_EMAIL, Toast.LENGTH_LONG).show()
+                                //Log.d("Log.d", "Exception : ${exception.errorCode}")
+                            }
+                        }
+                        /*
+                        if (task.exception is FirebaseAuthWeakPasswordException) {
+
+                            val exception = task.exception as FirebaseAuthWeakPasswordException
+                            Log.d("Log.d", "Exception : ${exception.errorCode}")
+                            if (exception.errorCode == FireBaseAuthConstants.ERROR_WEAK_PASSWORD) {
+                                Toast.makeText(activity, FireBaseAuthConstants.ERROR_WEAK_PASSWORD, Toast.LENGTH_LONG).show()
+                                //Log.d("Log.d", "Exception : ${exception.errorCode}")
+                            }
+                        }
+                        */
                     }
                 }
         }//проверить, что поля не пустые
@@ -34,7 +58,16 @@ class AccountHelper(activity: MainActivity) {
                     if (task.isSuccessful) {
                         activity.uiUpdate(task.result?.user) //показать email юзера
                     } else {
-                        Toast.makeText(activity, activity.resources.getString(R.string.sign_in_error), Toast.LENGTH_LONG).show()
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            Log.d("Log.d", "Exception : ${task.exception}")
+                            val exception = task.exception as FirebaseAuthInvalidCredentialsException
+
+                            if (exception.errorCode == FireBaseAuthConstants.ERROR_INVALID_EMAIL) {
+                                Toast.makeText(activity, FireBaseAuthConstants.ERROR_INVALID_EMAIL, Toast.LENGTH_LONG).show()
+                            } else if (exception.errorCode == FireBaseAuthConstants.ERROR_WRONG_PASSWORD) {
+                                Toast.makeText(activity, FireBaseAuthConstants.ERROR_WRONG_PASSWORD, Toast.LENGTH_LONG).show()
+                            }
+                        }
                     }
                 }
         }
@@ -43,7 +76,8 @@ class AccountHelper(activity: MainActivity) {
     private fun getSignInClient(): GoogleSignInClient {
         val googleSignInOptions = GoogleSignInOptions
             .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(activity.getString(R.string.default_web_client_id)).build()
+            .requestIdToken(activity.getString(R.string.default_web_client_id)).requestEmail()
+            .build()
         return GoogleSignIn.getClient(activity, googleSignInOptions)
     } // регистрация через гугл аккаунт
 
@@ -53,11 +87,14 @@ class AccountHelper(activity: MainActivity) {
         activity.startActivityForResult(intent, GoogleAccConst.GOOGLE_SIGN_IN_REQUEST_CODE)
     } // дальше пишем в Main Activity в onActivityResult
 
-    fun signInFirebaseWithGoogle(token:String) {
+    fun signInFirebaseWithGoogle(token: String) {
         val credential = GoogleAuthProvider.getCredential(token, null)
         activity.authentification.signInWithCredential(credential).addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Toast.makeText(activity, "Sign in done", Toast.LENGTH_LONG).show()
+                activity.uiUpdate(task.result?.user)
+            } else {
+                Log.d("Log.d", "Google Sign In Exception : ${task.exception}")
             }
         }
     } //запустить функцию из MainActivity
@@ -65,9 +102,17 @@ class AccountHelper(activity: MainActivity) {
     private fun sendEmailVerification(user: FirebaseUser) {
         user.sendEmailVerification().addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                Toast.makeText(activity, activity.resources.getString(R.string.send_verification_done), Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    activity,
+                    activity.resources.getString(R.string.send_verification_done),
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
-                Toast.makeText(activity, activity.resources.getString(R.string.send_verification_error), Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    activity,
+                    activity.resources.getString(R.string.send_verification_error),
+                    Toast.LENGTH_LONG
+                ).show()
             }
         } // успешно прошло или нет
     } // для отправки подтверждающего письма со ссылкой
